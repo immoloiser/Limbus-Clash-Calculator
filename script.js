c6c73be23b2c3e6d0c90e142a7e0f9452a9ee989
immoloiser <Max.Appleyard@protonmail.com>
2025-11-18 10:57:08 +0000

    Finished the enemy form and started on the javascript to calculate probabilities

---
+++ script.js
@@ -0,0 +1,18 @@
+function ClashCalculation(sbp,scp,ss,ebp,ecp,es)
+{
+    
+}
+function test(){
+    alert("Hello")
+}
+
+
+
+window.addEventListener("DOMContentLoaded", (event) => {
+    document.getElementById("Submitter").addEventListener("click", (event) => {
+    alert("Hello");
+    let Sform = new FormData(document.getElementById('form1'));
+    let Eform = new FormData(document.getElementById('form2'));
+    
+    });
+})
